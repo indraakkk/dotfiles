9204ab90d1f25416b134363c43f66a62ddb6965c
indra <indrakoslab@gmail.com>
2024-01-13 16:09:12 +0700

    chore: :construction: wsl home-manager__initial commit

---
+++ home.nix
@@ -0,0 +1,132 @@
+{ config, pkgs, ... }:
+{
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "indra";
+  home.homeDirectory = "/home/indra";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    # pkgs.bat
+    pkgs.nodejs_20
+    # pkgs.docker
+    # pkgs.colima
+    pkgs.fira-code
+    # pkgs.unar
+    # pkgs.nodePackages_latest.serve
+    # pkgs.openssl
+    # pkgs.python39
+    # pkgs.nodePackages_latest.localtunnel
+    pkgs.nodePackages_latest.pnpm
+    # pkgs.php82
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/indraalbd/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  # programs.git.enable = true;
+  # programs.vscode = {
+  #   enable = true;
+  #   extensions = with pkgs.vscode-extensions; [
+  #     dracula-theme.theme-dracula
+  #     yzhang.markdown-all-in-one
+  #   ];
+  # };
+
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "nix-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+          sha256 = "069ybzdj29s320wzdyxqjhmpm9ir5815yx6n522adav0z2nz8vs4";
+        };
+      }
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      command_timeout = 1000;
+      cmd_duration = {
+        format = " [$duration]($style) ";
+        style = "bold #EC7279";
+        show_notifications = true;
+      };
+      battery = {
+        full_symbol = "🔋 ";
+        charging_symbol = "⚡️ ";
+        discharging_symbol = "💀 ";
+      };
+      nodejs = {
+        format = "via [🤖 $version](bold green) ";
+      };
+    };
+  };
+
+  manual = {
+    html.enable = false;
+    manpages.enable = false;
+    json.enable = false;
+  };
+}
